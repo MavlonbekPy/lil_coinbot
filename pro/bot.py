322ac8a48cd06ae2fcdf17a41f1045eb31e20ff3
Bill <148610696+bilol9119@users.noreply.github.com>
2024-11-03 22:04:46 +0500

    Update bot.py

--- pro/bot.py
+++ pro/bot.py
@@ -13,7 +13,7 @@ def main_keyboard() -> InlineKeyboardMarkup:
             InlineKeyboardButton(
                 text="Play Game",
                 web_app=WebAppInfo(
-                    url="https://www.youtube.com/?app=desktop&hl=ru"
+                    url="https://localhost:8001"
                 )
             ),
             InlineKeyboardButton(
